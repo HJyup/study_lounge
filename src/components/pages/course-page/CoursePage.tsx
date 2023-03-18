911006bd48bd28cd4b2d6719b70280cbb5fa94a2
HJK <supermaster98z@gmail.com>
2023-03-18 15:38:19 +0000

    feature: added structure to Course Page

--- src/components/pages/course-page/CoursePage.tsx
+++ src/components/pages/course-page/CoursePage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from 'react-query';
-import { Alert } from '@mui/material';
+import { TimelapseOutlined } from '@mui/icons-material';
+import { Alert, CircularProgress } from '@mui/material';
 import { useRouter } from 'next/router';
 
 import { CoursersAPI } from '@/api/CoursesApi/CoursersAPI';
@@ -19,13 +20,39 @@ const CoursePage = () => {
   return (
     <div className={styles['page-container']}>
       <Navbar />
+      {isLoading && <CircularProgress />}
       {isError ? (
         <Alert variant="filled" severity="error">
           Finding problems with uploading data - Check later!
         </Alert>
       ) : (
         !isLoading && (
-          <div className={styles['card-course-container']}>{data?.id}</div>
+          <div className={styles['card-course-container']}>
+            <div className={styles['video-container']}>
+              <img
+                src={data?.previewImageLink + '/cover.webp'}
+                className={styles['image']}
+                alt={'Course preview image'}
+              />
+              {data?.lessons[0].title}
+            </div>
+            <div className={styles['information-container']}>
+              <div className={styles['list-content-title']}>
+                <p>Course Content</p>
+              </div>
+              {data?.lessons
+                .sort((s1, s2) => s1.order - s2.order)
+                .map((lesson, index) => (
+                  <div key={index} className={styles['list-content']}>
+                    <p>{lesson.order + '. ' + lesson.title}</p>
+                    <div className={styles['duration']}>
+                      <TimelapseOutlined color={'disabled'} />
+                      {lesson.duration + 'min'}
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
         )
       )}
     </div>
