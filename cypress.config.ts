95a02a97ae8f4e597b8520a5a7f12517cf18918d
HJK <supermaster98z@gmail.com>
2023-03-19 17:43:53 +0000

    fix: changed unit test navigation behaviour

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: '5f5ria',
   component: {
     devServer: {
       framework: 'next',
--- src/components/common/navbar/Navbar.cy.tsx
+++ src/components/common/navbar/Navbar.cy.tsx
@@ -3,14 +3,20 @@ import { mount } from 'cypress/react18';
 
 import Navbar from './Navbar';
 
+import styles from './Navbar.module.scss';
+
 describe('<Navbar />', () => {
   it('should render the navbar', () => {
     mount(<Navbar />);
-    cy.get('.MuiAppBar-root').should('exist');
+    cy.get('.' + styles['title']).should('exist');
+    cy.get('.' + styles['subtitle']).should('exist');
   });
 
   it('should display the app name', () => {
     mount(<Navbar />);
-    cy.get('.MuiTypography-subtitle1').contains('Study Lounge');
+    cy.get('.' + styles['title']).contains('Study Lounge');
+    cy.get('.' + styles['subtitle']).contains(
+      'Different knowledge in one place.',
+    );
   });
 });
