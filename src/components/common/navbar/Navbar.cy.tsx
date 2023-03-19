3988bda98ea9d31382d538dcadc39c6e35b17e1a
HJK <supermaster98z@gmail.com>
2023-03-19 21:28:24 +0000

    fix: fixed Navbar.tsx

--- src/components/common/navbar/Navbar.cy.tsx
+++ src/components/common/navbar/Navbar.cy.tsx
@@ -9,14 +9,10 @@ describe('<Navbar />', () => {
   it('should render the navbar', () => {
     mount(<Navbar />);
     cy.get('.' + styles['title']).should('exist');
-    cy.get('.' + styles['subtitle']).should('exist');
   });
 
   it('should display the app name', () => {
     mount(<Navbar />);
     cy.get('.' + styles['title']).contains('Study Lounge');
-    cy.get('.' + styles['subtitle']).contains(
-      'Different knowledge in one place.',
-    );
   });
 });
--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -3,15 +3,19 @@ import { AppBar } from '@mui/material';
 import { Toolbar } from '@mui/material';
 
 import styles from './Navbar.module.scss';
-const Navbar = () => (
+
+interface NavbarProps {
+  text?: string;
+}
+const Navbar: React.FC<NavbarProps> = ({
+  text = 'Different knowledge in one place',
+}) => (
   <div className={styles['navbar-container']}>
     <React.Fragment>
       <AppBar position={'fixed'} className={styles['navbar']}>
         <Toolbar className={styles['navbar-container']}>
           <div className={styles['title']}>Study Lounge</div>
-          <div className={styles['subtitle']}>
-            Different knowledge in one place.
-          </div>
+          <div className={styles['subtitle']}>{text}</div>
         </Toolbar>
       </AppBar>
     </React.Fragment>
